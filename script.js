76c41bdf4f0216c127c75020dd2219a772295dbf
aman17920 <90082894+aman17920@users.noreply.github.com>
2022-07-12 17:59:58 +0530

    Add ui

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
 
-
-
 var rect = $('#containerbox')[0].getBoundingClientRect();
 var mouse = {x: 0, y: 0, moved: false};
 
@@ -33,6 +31,47 @@ $(window).on('resize scroll', function(){
 
   
 })
+
+
+
+
+var rect = $('#containerbox')[0].getBoundingClientRect();
+var mouse = {x: 0, y: 0, moved: false};
+
+$("#containerbox").mousemove(function(e) {
+  mouse.moved = true;
+  mouse.x = e.clientX - rect.left;
+  mouse.y = e.clientY - rect.top;
+});
+ 
+// Ticker event will be called on every frame
+TweenLite.ticker.addEventListener('tick', function(){
+  if (mouse.moved){    
+    parallaxIt(".menu-pizza", -100);
+    parallaxIt(".menu-burger", -30);
+  }
+  mouse.moved = false;
+});
+
+function parallaxIt(target, movement) {
+  TweenMax.to(target, 0.5, {
+    x: (mouse.x - rect.width / 2) / rect.width * movement,
+    y: (mouse.y - rect.height / 2) / rect.height * movement
+  });
+}
+
+$(window).on('resize scroll', function(){
+  rect = $('#containerbox')[0].getBoundingClientRect();
+
+
+  
+})
+
+
+
+
+
+
 
   var swiper = new Swiper('.mySwiper', {
     // Default parameters
